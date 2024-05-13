74bad0beefbcfa20dee399e9c4f319a510f1748f
alan buscaglia <alanbuscaglia@gmail.com>
2024-05-13 15:13:27 +0200

    feature: fixed pdf download

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withVanillaExtract = createVanillaExtractPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	experimental: {
+		largePageDataBytes: 128 * 100000,
+	},
 	i18n: {
 		defaultLocale: 'en',
 		locales: ['en', 'es'],
